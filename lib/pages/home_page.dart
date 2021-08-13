4ca870bbd8f7ad25aa7de8f68d08bb09352218d5
Uekiz <kingghostdragon@gmail.com>
2021-08-13 17:03:59 +0700

    4

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:midterm_mobile/boxes.dart';
 import 'package:midterm_mobile/main.dart';
 import 'package:midterm_mobile/models/lasttime.dart';
+import 'package:intl/intl.dart';
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
@@ -13,7 +16,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   final List<LastTime> lasttimelist = [];
 
   @override
@@ -23,9 +25,50 @@ class _MyHomePageState extends State<MyHomePage> {
     super.dispose();
   }
 
-
   @override
   Widget build(BuildContext context) {
-    return MyScaffold(route: '/', body: Text('home'),);
+    return MyScaffold(
+        route: '/',
+        body: ValueListenableBuilder<Box<LastTime>>(
+            valueListenable: Boxes.getLastTime().listenable(),
+            builder: (context, box, _) {
+              final lasttimelist = box.values.toList().cast<LastTime>();
+
+              return Column(
+                children: [
+                  Expanded(
+                    child: ListView.builder(
+                      padding: EdgeInsets.all(8),
+                      itemCount: lasttimelist.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        final lasttime = lasttimelist[index];
+
+                        final date = DateFormat.MMMMd().format(lasttime.lastday);
+
+                        return Card(
+                          color: Colors.white,
+                          child: ExpansionTile(
+                            tilePadding:
+                                EdgeInsets.symmetric(horizontal: 24, vertical: 8),
+                            title: Text(
+                              lasttime.title,
+                              maxLines: 2,
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 18),
+                            ),
+                            subtitle: Text(date),
+                            trailing: Text(
+                              lasttime.group,
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 16),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              );
+            }));
   }
 }
